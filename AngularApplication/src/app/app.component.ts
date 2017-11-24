6f73f9b20aabc6b87e457183dc986569dfa93dd6
Todor Ivanov <todor.ivanov@primeholding.com>
2017-11-24 17:42:52 +0200

    Integrate live chat

--- AngularApplication/src/app/app.component.ts
+++ AngularApplication/src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Intercom } from 'ng-intercom';
+
 
 @Component({
   selector: 'lpp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+
+  constructor(private intercom: Intercom) {
+
+  }
+
+  ngOnInit() {
+    this.intercom.boot({
+      app_id: "n1npujem"
+    });
+  }
+}
