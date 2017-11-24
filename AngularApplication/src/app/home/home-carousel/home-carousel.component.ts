27c086aed8b4056b5fabef70df90f53cac9ede92
Todor Atanasov <todor.atanasov@primeholding.com>
2017-11-24 17:12:21 +0200

    LPP-11 Homepage styles - product carousel page

---
+++ AngularApplication/src/app/home/home-carousel/home-carousel.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NgxCarousel } from 'ngx-carousel';
+
+@Component({
+  selector: 'lpp-home-carousel',
+  templateUrl: './home-carousel.component.html',
+  styleUrls: ['./home-carousel.component.scss']
+})
+export class HomeCarouselComponent implements OnInit {
+
+  carouselOptions: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.carouselOptions = {
+      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: true,
+      loop: true,
+      custom: 'banner'
+    };
+  }
+
+}
