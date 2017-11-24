b67f3fdc8e1c4b9d0aae559f68838a9ab3c586fa
Todor Atanasov <todor.atanasov@primeholding.com>
2017-11-24 17:12:21 +0200

    LPP-11 Homepage styles - product carousel page

--- AngularApplication/src/app/app.module.ts
+++ AngularApplication/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxCarouselModule } from 'ngx-carousel';
+import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeCarouselComponent } from './home/home-carousel/home-carousel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeCarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
