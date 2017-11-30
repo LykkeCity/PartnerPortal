1f2b08d8f1da9b567a8cfb7f57bae61419806e21
Dafinka Gerdjikova <dafinka.gerdjikova@programista.pro>
2017-11-30 09:19:31 +0200

    Abstract email sending, add  validations for e contacts pop up requests

---
+++ LykkePartnerPortal/Helpers/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using LykkePartnerPortal.Models.EmailTemplates;
+using LykkePartnerPortal.Settings;
+
+namespace LykkePartnerPortal.Helpers
+{
+    public interface IEmailSender
+    {
+        void SendEmail(IEmailTemplate model, EmailCredentialsSettings settings, string templateName, string subject);
+    }
+}
---
+++ LykkePartnerPortal/Models/Validations/ContactRequestValidationModel.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using LykkePartnerPortal.Models.Contacts;
+using LykkePartnerPortal.Strings;
+
+namespace LykkePartnerPortal.Models.Validations
+{
+    public class ContactRequestValidationModel : AbstractValidator<ContactRequestModel>
+    {
+        public ContactRequestValidationModel()
+        {
+            RuleFor(reg => reg.Email).NotNull().WithMessage(Phrases.FieldShouldNotBeEmpty);
+            RuleFor(reg => reg.Email).EmailAddress().WithMessage(Phrases.InvalidEmailFormat);
+
+            RuleFor(reg => reg.Source).NotNull().WithMessage(Phrases.FieldShouldNotBeEmpty);
+            RuleFor(reg => reg.Source).NotEmpty().WithMessage(Phrases.FieldShouldNotBeEmpty);
+
+            RuleFor(reg => reg.FullName).NotNull().WithMessage(Phrases.FieldShouldNotBeEmpty);
+            RuleFor(reg => reg.FullName).NotEmpty().WithMessage(Phrases.FieldShouldNotBeEmpty);
+
+            RuleFor(reg => reg.PhoneNumber).NotNull().WithMessage(Phrases.FieldShouldNotBeEmpty);
+            RuleFor(reg => reg.PhoneNumber).NotEmpty().WithMessage(Phrases.FieldShouldNotBeEmpty);
+        }
+    }
+}
