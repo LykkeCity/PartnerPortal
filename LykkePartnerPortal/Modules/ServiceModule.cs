a056bfd571b00869cbcd6e21d393fcf5c722d9cb
Dafinka Gerdjikova <dafinka.gerdjikova@programista.pro>
2017-12-06 15:20:07 +0200

    Add endpoint for SSO token response

--- LykkePartnerPortal/Modules/ServiceModule.cs
+++ LykkePartnerPortal/Modules/ServiceModule.cs
@@ -31,6 +31,8 @@ namespace LykkePartnerPortal.Modules
             RegisterExternalServices(builder);
 
             builder.RegisterInstance(_settings.CurrentValue.LykkePartnerPortal.EmailCredentials);
+            builder.RegisterInstance(_settings.CurrentValue.LykkePartnerPortal.Authentication);
+
             builder.Populate(_services);
         }
 
