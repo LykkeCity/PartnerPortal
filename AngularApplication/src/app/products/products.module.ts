ff2b71e4a4ab813ed22e80e12b7e1e45174dd7c4
Todor Ivanov <todor.ivanov@primeholding.com>
2017-12-07 16:14:41 +0200

    Add user image and fix responsive styling

--- AngularApplication/src/app/products/products.module.ts
+++ AngularApplication/src/app/products/products.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PopoverModule } from 'ngx-bootstrap';
+import { SharedModule } from '../shared/shared.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductsItemComponent } from './products-item/products-item.component';
 
@@ -10,7 +12,9 @@ import { ProductsItemComponent } from './products-item/products-item.component';
     ProductsItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    PopoverModule.forRoot()
   ]
 })
 export class ProductsModule { }
