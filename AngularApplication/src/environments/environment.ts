93d322d9ada1912d01f8b079af6d19ab974f3f99
Todor Atanasov <todor.atanasov@primeholding.com>
2017-12-08 12:24:03 +0200

    Proxy requests to the dev API to avoid cross-origin requests errors

--- AngularApplication/src/environments/environment.ts
+++ AngularApplication/src/environments/environment.ts
@@ -5,6 +5,7 @@
 
 export const environment = {
   production: false,
+  apiUrl: 'http://localhost:4200/devapi',
   apiAuthUrl: 'https://auth-dev.lykkex.net',
   applicationId: 'dbe670d7-0732-4f41-b27a-09de1e7c0ecd',
   redirectUrl: 'http://localhost:4200/',
