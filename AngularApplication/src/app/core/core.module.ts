e8935fd73d8032a2d4cb8aba553a5d42957954f0
Todor Atanasov <todor.atanasov@primeholding.com>
2017-12-08 11:58:21 +0200

    Core module singleton loading

--- AngularApplication/src/app/core/core.module.ts
+++ AngularApplication/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsletterSubscriptionService } from './newsletter-subscription.service';
 import { AuthService } from './auth.service';
@@ -8,13 +8,28 @@ import { LoginRedirectGuard } from './login-redirect.guard';
 @NgModule({
   imports: [
     CommonModule
-  ],
-  declarations: [],
-  providers: [
-    AuthGuard,
-    AuthService,
-    LoginRedirectGuard,
-    NewsletterSubscriptionService
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        AuthGuard,
+        AuthService,
+        LoginRedirectGuard,
+        NewsletterSubscriptionService
+      ]
+    };
+  }
+
+  constructor (
+    @Optional() @SkipSelf() parentModule: CoreModule,
+  ) {
+    // throw in case someone wrongly imports this module twice
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
