8a55c539a6ed43de9478c2191c6a94142a2f5c03
Todor Atanasov <todor.atanasov@primeholding.com>
2017-12-12 14:33:11 +0200

    Moved products page routing to products module

--- AngularApplication/src/app/app-routing.module.ts
+++ AngularApplication/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginRedirectGuard } from './core/login-redirect.guard';
-import { ProductsComponent } from './products/products/products.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [ LoginRedirectGuard ] },
-  { path: 'products', component: ProductsComponent }
+  { path: '', component: HomeComponent, canActivate: [ LoginRedirectGuard ] }
 ];
 
 @NgModule({
--- AngularApplication/src/app/products/products.module.ts
+++ AngularApplication/src/app/products/products.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductsItemComponent } from './products-item/products-item.component';
+import { ProductsRootComponent } from './products-root/products-root.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductsItemComponent
+    ProductsItemComponent,
+    ProductsRootComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    ProductsRoutingModule,
   ]
 })
 export class ProductsModule { }
