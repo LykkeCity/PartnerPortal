c6b922e6030a38dfecc574d374a2c05b23cbeed4
Todor Atanasov <todor.atanasov@primeholding.com>
2017-12-15 13:18:52 +0200

    Introduced local, dev and prod environment in Angular app

--- AngularApplication/src/environments/environment.prod.ts
+++ AngularApplication/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiAuthUrl: '',
-  applicationId: '',
+  apiUrl: 'https://api-dev.lykkex.net/api/',
+  apiAuthUrl: 'https://auth-dev.lykkex.net',
+  applicationId: '', // TODO: generate appId and put it here
   redirectUrl: 'https://partners.lykke.com/',
 };
